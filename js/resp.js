d32b537dc57e4029ea905013d5a36ebc4b0cbb90
moiz333222111 <150353393+moiz333222111@users.noreply.github.com>
2023-11-10 16:24:21 +0500

    MY WEBSITE

---
+++ js/resp.js
@@ -0,0 +1,18 @@
+burger = document.querySelector('burger')
+navbar = document.querySelector('navbar')
+rightnav = document.querySelector('rightnav')
+navlist = document.querySelector('navlist')
+
+burger.addEventListener('click', ()=>{
+        rightnav.classList.toggle('v-class');
+        navlist.classList.toggle('v-class');
+        navbar.classList.toggle('h-nav');
+    });
+  
+  
+  
+
+
+
+
+
